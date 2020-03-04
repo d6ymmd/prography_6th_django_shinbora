fb7406237112e07d071ac5f777b36032b6d44e8a
bora <d6ymmd@gmail.com>
2020-03-04 22:29:24 +0900

    APIView 수정

--- blog/urls.py
+++ blog/urls.py
@@ -12,8 +12,5 @@ app_name = 'blog'
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.ArticleList.as_view(), name='article-list'),
-    path('create/', views.ArticleCreate.as_view(), name='article-create'),
     path('<int:pk>/', views.ArticleDetail.as_view(), name='article-detail'),
-    path('<int:pk>/update/', views.ArticleUpdate.as_view(), name='article-update'),
-    path('<int:pk>/delete/', views.ArticleDelete.as_view(), name='article-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,15 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleList(ListAPIView):
+class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all().order_by('-created_at')
     serializer_class = ArticleSerializer
 
 
-class ArticleDetail(RetrieveAPIView):
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-
-class ArticleUpdate(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleDelete(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleCreate(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
 
